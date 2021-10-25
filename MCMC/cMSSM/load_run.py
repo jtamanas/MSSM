958501b5a16ad9a514176d6f82f31d6d2f7bbb59
John Tamanas <8213449+jtamanas@users.noreply.github.com>
2021-10-25 09:53:10 -0700

    Fixed backend issues

---
+++ MCMC/cMSSM/load_run.py
@@ -0,0 +1,36 @@
+import corner
+import emcee
+import numpy as np
+import matplotlib.pyplot as plt
+from mcmc_settings import mcmc_settings
+
+
+theta_dim = 4
+
+filename = f"runs/{mcmc_settings['nwalkers']}walkers_{mcmc_settings['max_n']}sampleseach_{mcmc_settings['simulator']}.h5"
+reader = emcee.backends.HDFBackend(filename)
+
+tau = reader.get_autocorr_time(tol=0)
+burnin = int(2 * np.max(tau))
+thin = max(1, int(0.5 * np.min(tau)))
+
+samples = reader.get_chain(discard=burnin, flat=True, thin=thin)
+log_prob_samples = reader.get_log_prob(discard=burnin, flat=True, thin=thin)
+log_prior_samples = reader.get_blobs(discard=burnin, flat=True, thin=thin)
+
+print("burn-in: {0}".format(burnin))
+print("thin: {0}".format(thin))
+print("flat chain shape: {0}".format(samples.shape))
+print("flat log prob shape: {0}".format(log_prob_samples.shape))
+print("flat log prior shape: {0}".format(log_prior_samples.shape))
+
+all_samples = np.concatenate(
+    (samples, log_prob_samples[:, None], log_prior_samples[:, None]), axis=1
+)
+
+labels = list(map(r"$\theta_{{{0}}}$".format, range(1, theta_dim + 1)))
+labels = [r"$M_0$", r"$M_{1/2}$", r"$A_0$", r"$\tan\beta$"]
+labels += ["log prob", "log prior"]
+
+corner.corner(all_samples, labels=labels)
+plt.show()
---
+++ MCMC/cMSSM/main.py
@@ -0,0 +1,117 @@
+import emcee
+from emcee.autocorr import AutocorrError
+import numpy as np
+from lbi.prior import SmoothedBoxPrior
+import corner
+import matplotlib.pyplot as plt
+from mcmc_settings import mcmc_settings
+import sys
+from tqdm.auto import tqdm
+
+sys.path.append("/media/jt/data/Projects/MSSM/micromegas_5.2.7.a/MSSM/")
+from pymicromegas import MicromegasSettings, SugraParameters
+from spheno import spheno
+from softsusy import softsusy
+micromegas = {"spheno": spheno, "softsusy": softsusy}
+
+
+def sim_wrapper(theta):
+    params = [SugraParameters(*th) for th in theta]
+    results = micromegas[mcmc_settings["simulator"]](params=params, settings=settings)
+    out = np.array([results.omega(), results.gmuon(), results.mhsm()])
+    out = out.T
+    # print(out)
+    return out
+
+
+def log_likelihood(unitless_theta):
+    theta = theta_addunits(unitless_theta)
+    th_obs = sim_wrapper(theta)
+    lnp = -0.5 * np.sum(((th_obs - exp_obs) / sigma_obs) ** 2, axis=1)
+    return np.nan_to_num(lnp, nan=-np.inf)
+
+
+def log_posterior(unitless_theta):
+    unitless_theta = np.atleast_2d(unitless_theta)
+    log_prior_pdf = unitless_prior(unitless_theta).sum(axis=1)
+    log_prob = log_likelihood(unitless_theta) + log_prior_pdf
+    return log_prob, log_prior_pdf
+
+
+def theta_addunits(unitless_theta):
+    theta = np.zeros_like(unitless_theta)
+    # WARNING: changes unitless_theta in place
+    theta[:, 0] = 1000 * unitless_theta[:, 0] * 10.0
+    theta[:, 1] = 1000 * unitless_theta[:, 1] * 10.0
+    theta[:, 2] = 12 * unitless_theta[:, 0] - 6 * unitless_theta[:, 0]
+    theta[:, 3] = 48.5 * unitless_theta[:, 3] + 1.5
+    theta = np.hstack((theta, np.ones((len(theta), 1))))
+    return theta
+
+
+# default settings
+theta_dim = 4
+settings = MicromegasSettings(
+    relic_density=True,
+    masses=True,
+    gmuon=True,
+    bsg=True,
+    bsmumu=True,
+    btaunu=True,
+    delta_rho=True,
+    sort_odd=True,
+    fast=True,
+    beps=0.0001,
+    cut=0.01,
+)
+
+
+unitless_prior, _ = SmoothedBoxPrior(theta_dim=theta_dim, lower=0, upper=1, sigma=0.01)
+
+exp_obs = np.array([0.12, 2e-11, 125.0])
+sigma_obs = np.array([0.06, 5e-11, 5.0])
+
+coords = np.random.rand(mcmc_settings["nwalkers"], theta_dim)
+
+
+# Set up the backend
+# Don't forget to clear it in case the file already exists
+filename = f"runs/{mcmc_settings['nwalkers']}walkers_{mcmc_settings['max_n']}sampleseach_{mcmc_settings['simulator']}.h5"
+backend = emcee.backends.HDFBackend(filename)
+backend.reset(mcmc_settings["nwalkers"], theta_dim)
+
+# Initialize the sampler
+sampler = emcee.EnsembleSampler(
+    mcmc_settings["nwalkers"],
+    theta_dim,
+    log_posterior,
+    backend=backend,
+)
+
+
+# We'll track how the average autocorrelation time estimate changes
+index = 0
+autocorr = np.empty(mcmc_settings["max_n"])
+
+# This will be useful to testing convergence
+old_tau = np.inf
+
+# Now we'll sample for up to max_n steps
+for sample in sampler.sample(coords, iterations=mcmc_settings["max_n"], progress=True):
+    # Only check convergence every 100 steps
+    if sampler.iteration % 100:
+        continue
+
+    # Compute the autocorrelation time so far
+    # Using tol=0 means that we'll always get an estimate even
+    # if it isn't trustworthy
+    tau = sampler.get_autocorr_time(tol=0)
+    autocorr[index] = np.mean(tau)
+    index += 1
+
+    # Check convergence
+    converged = np.all(tau * 100 < sampler.iteration)
+    converged &= np.all(np.abs(old_tau - tau) / tau < 0.01)
+    if converged:
+        break
+    old_tau = tau
